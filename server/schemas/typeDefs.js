c6af3d7f016c6c19b26b329a11457217a8377f93
zelstart <e.pomeroy95@gmail.com>
2023-11-22 17:40:35 -0500

    define necessary query and mutation types

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const typeDefs =`
+type Query {
+    users: [User]
+    user(username: String!): User
+    me: User
+}
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth 
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User  
+}
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+`
