4d130f5aee5f5c3f83c2751ff7dfe09cb70a045e
zelstart <e.pomeroy95@gmail.com>
2023-11-22 16:30:36 -0500

    Add Apollo Server and GraphQL middleware

--- server/server.js
+++ server/server.js
@@ -2,12 +2,35 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+// import ApolloServer and gql from apollo-server-express
+const { ApolloServer, gql } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// start Apollo server
+const startApolloServer = async () => {
+  await server.start();
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// apply middleware to the /graph endpoint
+app.use('/graphql', expressMiddleware(server, {
+  context: authMiddleware,
+})
+);
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -19,3 +42,6 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+// start Apollo server
+startApolloServer();
