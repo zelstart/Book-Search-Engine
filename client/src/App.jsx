2dfd8be3cf2b4504b346e286ca4679499fdf5417
zelstart <e.pomeroy95@gmail.com>
2023-11-22 18:14:40 -0500

    added Apollo Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,42 @@
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@ApolloClient';
+import { setContext } from '@ApolloLinkContext';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+// construct main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
