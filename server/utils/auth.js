c4b461dd43803348898093a72b8060a2403054c9
zelstart <e.pomeroy95@gmail.com>
2023-11-22 16:13:39 -0500

    updated the auth to work with GraphQL

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { GraphQLError } = require('graphql');
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
@@ -6,34 +7,29 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via  req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
+    // if no token, return request object as is
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      throw new Error('You have no token!');
     }
 
     // verify token and get user data out of it
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+      const { user } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = user;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
-
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    
+    // return updated request object
+    return req;
   },
 };
